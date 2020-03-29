ac66d23f901723ce47266ebb6ce5af12152e4645
khizer <moh.khizer61@gmail.com>
2020-03-29 08:48:59 -0700

    Updates

---
+++ precache-manifest.6bb6480c8f4871fcacb38e57b17f8d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b317b47278f327ae42a71a2fbc97f4d",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "a8606a84e5a7fdac6d1c",
+    "url": "/monster/static/css/main.4433313e.chunk.css"
+  },
+  {
+    "revision": "843682a76d5bd88c9a3f",
+    "url": "/monster/static/js/2.e3747a72.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster/static/js/2.e3747a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8606a84e5a7fdac6d1c",
+    "url": "/monster/static/js/main.a72feb39.chunk.js"
+  },
+  {
+    "revision": "2aef560db3c106cdda40",
+    "url": "/monster/static/js/runtime-main.4fe1648d.js"
+  }
+]);
